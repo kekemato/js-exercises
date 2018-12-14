c8dfb1f22dd22edc7845e76d3f916b58022e1a39
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-14 12:04:34 +0100

    task 5 - changed tasks to accept infinite arguments and nt array

--- tasks.js
+++ tasks.js
@@ -12,17 +12,11 @@ console.log(arithmeticAverage(array))
 const median = arr => {
     let median = 0
     let = numsLength = arr.length;
-    numbers.sort((a, b) => a - b)
+    arr.sort((a, b) => a - b)
 
-    if (
-        numsLength % 2 === 0
-    ) {
-        median = (arr[numsLength / 2 - 1] + arr[numsLength / 2]) / 2;
-    } else {
-        median = arr[(numsLength - 1) / 2];
-    }
-
-    return median;
+        return numsLength % 2 === 0 ?
+        ((arr[numsLength / 2 - 1] + arr[numsLength / 2]) / 2)
+        : (arr[(numsLength - 1) / 2])
 }
 
 console.log(median(array))
@@ -46,3 +40,50 @@ const maxNumber = arr => Math.max(...arr)
 console.log(maxNumber(array))
 
 // -----------------------------
+
+// const arithmeticAverageWithoutArray = () => {
+//     const sum = 0
+//     for (let i = 0; i < arguments.length; i++){
+//         sum += arguments[i]
+//     }
+//     return result = sum / arguments.length
+// }
+
+const arithmeticAverageWithoutArray = (...arr) => {
+    const sum = arr.reduce((reduced, element) => reduced + element, 0)
+    return result = sum / arr.length
+}
+
+console.log(arithmeticAverageWithoutArray(1, 2, 3, 5, 7))
+
+// -----------------------------
+
+const medianWithoutArray = (...arr) => {
+    let median = 0
+    let = numsLength = arr.length;
+    arr.sort((a, b) => a - b)
+
+    if (
+        numsLength % 2 === 0
+    ) {
+        median = (arr[numsLength / 2 - 1] + arr[numsLength / 2]) / 2;
+    } else {
+        median = arr[(numsLength - 1) / 2];
+    }
+
+    return median;
+}
+
+console.log(medianWithoutArray(1, 3, 5, 7, 8))
+
+// -----------------------------
+
+const minNumberWithoutArray = (...arr) => Math.min(...arr)
+
+console.log(minNumberWithoutArray(2, 3, 5, 7, 8))
+
+// -----------------------------
+
+const maxNumberWithoutArray = (...arr) => Math.max(...arr)
+
+console.log(maxNumberWithoutArray(1, 3, 5, 7, 8))
